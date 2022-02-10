ff9bc8f6d3003f55685efbd0e5be78fe12305a40
Oliver Temple <oliver.temple.dev@gmail.com>
2022-02-10 21:39:37 +0000

    added "-h" help command

--- cdd.bat
+++ cdd.bat
@@ -1,12 +1,18 @@
 @echo off
-if %1 == -a (
-    setx %2 %3
+
+if %1== -h (
+    echo "Usage: cdd.bat [-a (add)|-d (delete)] [name] [path]"
 ) else (
-    if %1 == -d (
-        reg delete "HKCU\Environment" /v %2 /f
+    if %1 == -a (
+        setx %2 %3
     ) else (
-        FOR /F "usebackq tokens=3*" %%A IN (`reg query "HKCU\Environment" /v %1`) DO (
-            cd %%A %%B
+        if %1 == -d (
+            reg delete "HKCU\Environment" /v %2 /f
+        ) else (
+            FOR /F "usebackq tokens=3*" %%A IN (`reg query "HKCU\Environment" /v %1`) DO (
+                cd %%A %%B
+            )
         )
     )
 )
+
